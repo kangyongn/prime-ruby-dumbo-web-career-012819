74757ac363de0692821fdd6ddcb314a1d2889114
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-09 07:31:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,8 +3,9 @@ def prime? (number)
   # by definition prime number is a whole number greater than 1.
   false if number <= 1
   
-  
+  # 2, 3 is a prime number.
   true if number <= 3
+  
   
   
 end
