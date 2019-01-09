266863896de5ae2774216278fa2b3560a892b139
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-09 07:42:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,18 +1,10 @@
 def prime? (number)
   
   # by definition prime number is a whole number greater than 1.
-  return false if number <= 1
+  if number <= 1
+    false
   
   # 2, 3 is a prime number.
   return true if number <= 3
-  
-  # check for efficiency.
-  return false if number % 2 == 0 || number % 3 == 0
-  
-  # counter.
-  i = 5
-  while number ** 2 <= number
-    return false if n % i == 0 || n % (i + 2) == 0
-    i = i + 6
-  end
+
 end
